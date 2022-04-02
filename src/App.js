5866385c7566e450f6bde806d7b414cd8632e0bb
Ubuntu <ubuntu@ip-172-31-72-110.ec2.internal>
2022-04-02 13:32:06 +0000

    add new test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TestimonialsSection from "./components/testimonials/testimonials";
 function App() {
   return (
     <div className="App">
+     <small>Educative </small>
       <TestimonialsSection />
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,17 @@
-test("renders learn react link", () => {
-  expect(true).toBe(true);
+import { render, screen } from '@testing-library/react'; 
+import App from './App'; 
+import Testimonial from './components/testimonials/testimonials'
+
+test('renders learn main component', () => {   
+render(<App />);   
+const linkElement = screen.getByText(/Educative/i);   
+expect(linkElement).toBeInTheDocument()
+});
+
+
+test('test the testimonial components', () => {
+render(<Testimonial />);
+const testme = screen.getByText(/UX Engineer/i);
+expect(testme).toBeInTheDocument();
+
 });
